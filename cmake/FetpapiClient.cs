e97a82d322df1e227ff5d2eed03a0c48b8e0f297
mpoudret <mathieu.poudret@f2i-consulting.com>
2021-03-17 10:41:09 +0100

    Add .Net client

---
+++ cmake/FetpapiClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using F2iConsulting.Fetpapi;
+using F2iConsulting.Fetpapi.${FETPAPI_ETP_NS};
+
+namespace FetpapiClient
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            PlainClientSession session = fetpapi.createWsClientSession("127.0.0.1", "8080", "/", "");
+            session.setCoreProtocolHandlers(new MyOwnClientCoreHandlers(session));
+            session.setDiscoveryProtocolHandlers(new MyOwnDiscoveryProtocolHandlers(session));
+            session.setStoreProtocolHandlers(new MyOwnStoreProtocolHandlers(session));
+            session.run();
+            Console.WriteLine("FINISHED");
+        }
+    }
+}
